742ea845aa0add816aff2823b122b82af86a7437
Cristian Quiroz <cris@qrz.io>
2019-11-04 10:30:51 +0000

    Initial commit with module structure

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Ampersand_DisableStockReservation',
+    __DIR__
+);
+
+
+
