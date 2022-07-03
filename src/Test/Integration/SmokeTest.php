84effdd634760467ef48a5a4d7f49e8c5cc51256
Damien Retzinger <damienwebdev@gmail.com>
2022-07-03 13:16:50 -0400

    ci: add unit, install and integration test workflows

---
+++ src/Test/Integration/SmokeTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ampersand\DisableStockReservation\Test\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+class SmokeTest extends TestCase
+{
+    public function testItworks()
+    {
+        $this->assertEquals(true, true);
+    }
+}
