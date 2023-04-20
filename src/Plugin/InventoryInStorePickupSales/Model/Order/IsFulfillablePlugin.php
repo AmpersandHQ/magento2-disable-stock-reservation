d8a3bb3721f383251aa3c9c17445141c42a275bc
Waleed <waleed@customgento.com>
2023-04-20 10:28:03 +0200

    Changes made as per suggestion of Pull Request

--- src/Plugin/InventoryInStorePickupSales/Model/Order/IsFulfillablePlugin.php
+++ src/Plugin/InventoryInStorePickupSales/Model/Order/IsFulfillablePlugin.php
@@ -1,11 +1,13 @@
 <?php
+declare(strict_types=1);
 
 namespace Ampersand\DisableStockReservation\Plugin\InventoryInStorePickupSales\Model\Order;
+use Magento\InventoryInStorePickupSales\Model\Order\IsFulfillable;
 
 class IsFulfillablePlugin
 {
-    public function aroundExecute()
+    public function afterExecute(IsFulfillable $subject, $result)
     {
-        return true;
+        return '|' . true . '|';
     }
 }
