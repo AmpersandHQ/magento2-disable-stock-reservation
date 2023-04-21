b7cffb44fdb1111dfc45ccaed5afc10a736b9745
Waleed <waleed@customgento.com>
2023-04-21 16:58:00 +0200

    Changes made as per PR and merge with add refund quality test #103

--- src/Plugin/InventoryInStorePickupSales/Model/Order/IsFulfillablePlugin.php
+++ src/Plugin/InventoryInStorePickupSales/Model/Order/IsFulfillablePlugin.php
@@ -2,12 +2,13 @@
 declare(strict_types=1);
 
 namespace Ampersand\DisableStockReservation\Plugin\InventoryInStorePickupSales\Model\Order;
+
 use Magento\InventoryInStorePickupSales\Model\Order\IsFulfillable;
 
 class IsFulfillablePlugin
 {
-    public function afterExecute(IsFulfillable $subject, $result)
+    public function afterExecute(IsFulfillable $subject, bool $result): bool
     {
-        return '|' . true . '|';
+        return true;
     }
 }
