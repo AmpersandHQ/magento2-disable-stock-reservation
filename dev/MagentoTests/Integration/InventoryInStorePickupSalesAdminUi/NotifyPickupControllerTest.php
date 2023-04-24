11ce947b31a615a327eb866204e1be25cadb6202
Luke Rodgers <lr@amp.co>
2023-04-24 17:25:37 +0100

    Prepare for integration test

---
+++ dev/MagentoTests/Integration/InventoryInStorePickupSalesAdminUi/NotifyPickupControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+namespace Ampersand\DisableStockReservation\Test\Integration\InventoryInStorePickupSalesAdminUi;
+
+/**
+ * @magentoAppArea adminhtml
+ */
+class NotifyPickupControllerTest extends \Magento\TestFramework\TestCase\AbstractBackendController
+{
+    public function testNotifyProducts()
+    {
+        // TODO use tddwizard/magento2-fixtures to generate a product, an order, and then run this controller against it and assert against the results
+        $this->assertFalse(false);
+    }
+}
