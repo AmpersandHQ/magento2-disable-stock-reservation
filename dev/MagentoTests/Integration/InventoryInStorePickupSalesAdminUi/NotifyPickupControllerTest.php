6dceacb7d60ae5499a84f9a386f841229b5b2c8a
Luke Rodgers <lr@amp.co>
2023-04-24 20:18:48 +0100

    Identify affected versions

--- dev/MagentoTests/Integration/InventoryInStorePickupSalesAdminUi/NotifyPickupControllerTest.php
+++ dev/MagentoTests/Integration/InventoryInStorePickupSalesAdminUi/NotifyPickupControllerTest.php
@@ -10,6 +10,6 @@ class NotifyPickupControllerTest extends \Magento\TestFramework\TestCase\Abstrac
     public function testNotifyProducts()
     {
         // TODO use tddwizard/magento2-fixtures to generate a product, an order, and then run this controller against it and assert against the results
-        $this->assertFalse(false);
+        $this->assertTrue(class_exists(\Magento\InventoryInStorePickupSales\Model\Order\IsFulfillable::class));
     }
 }
