668a7f3c0ba10f8ec3e4c90d9d24c81aecc745d0
Luke Rodgers <lr@amp.co>
2023-05-23 14:33:08 +0100

    debug integration tests

--- dev/MagentoTests/Integration/MultipleSourceInventoryTest.php
+++ dev/MagentoTests/Integration/MultipleSourceInventoryTest.php
@@ -123,19 +123,18 @@ class MultipleSourceInventoryTest extends TestCase
 
 
     /**
-     * @magentoDbIsolation enabled
      * @magentoConfigFixture current_store cataloginventory/indexer/strategy async
      * @dataProvider sourcesDataProvider
-     *
-     * @magentoDataFixtureBeforeTransaction Magento_InventorySalesApi::Test/_files/websites_with_stores.php
      * @magentoDataFixture Magento_InventoryApi::Test/_files/products.php
      * @magentoDataFixture Magento_InventoryApi::Test/_files/sources.php
      * @magentoDataFixture Magento_InventoryApi::Test/_files/stocks.php
      * @magentoDataFixture Magento_InventoryApi::Test/_files/stock_source_links.php
      * @magentoDataFixture Magento_InventoryApi::Test/_files/source_items.php
+     * @magentoDataFixtureBeforeTransaction Magento_InventorySalesApi::Test/_files/websites_with_stores.php
      * @magentoDataFixture Magento_InventorySalesApi::Test/_files/stock_website_sales_channels.php
      * @magentoDataFixture Magento_InventorySalesApi::Test/_files/quote.php
      * @magentoDataFixture Magento_InventoryIndexer::Test/_files/reindex_inventory.php
+     * @magentoDbIsolation disabled
      *
      * @throws LocalizedException
      * @throws \Exception
